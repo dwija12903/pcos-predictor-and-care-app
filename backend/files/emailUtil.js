b48356751fb29958b74876f07cf45961cc7ca14a
Dwija Panchal <dwija1209@gmail.com>
2024-10-05 16:44:07 +0530

    Update emailUtil.js

--- backend/files/emailUtil.js
+++ backend/files/emailUtil.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 async function sendEmail(to, subject, html) {
     let transporter = nodemailer.createTransport({
@@ -6,13 +7,13 @@ async function sendEmail(to, subject, html) {
         port: 465,      // Port for SMTP (usually 465)
         secure: true,   // Use SSL
         auth: {
-            user: '', // Your email
-            pass: ' ', // Your email password or app password
+            user: process.env.EMAIL_ADDRESS, 
+            pass: process.env.EMAIL_APP_PASSWORD,
         },
     });
 
     let info = await transporter.sendMail({
-        from: '"PCOS Predictor & Care App" < >', // Sender address
+        from: `"PCOS Predictor & Care App" <${process.env.EMAIL_ADDRESS}>`, // Sender address
         to: to,             // Receiver's email
         subject: subject,   // Subject line
         html: html,         // HTML body
